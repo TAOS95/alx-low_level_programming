fe6f5e4386d5407faa3cb401e312406cb33f3102
TAOS95 <tas95245@gmail.com>
2022-10-07 17:42:14 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
+ */
+void *malloc_checked(unsigned int b)
+{
+void *point = malloc(b);
+
+if (point == NULL)
+exit(98);
+  
+return (point);
+}
