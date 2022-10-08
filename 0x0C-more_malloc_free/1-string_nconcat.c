246d1ca73a8593abf54452473394e6fd80901315
TAOS95 <tas95245@gmail.com>
2022-10-07 18:21:24 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *arr;
-  unsigned int i, j, co, co_2;
+char *arr;
+unsigned int i, j, co, co_2;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-  for (i = 0; s1[i] != '\0'; i++)
-    {
-    }
+for (i = 0; s1[i] != '\0'; i++)
+{
+}
 
-  for (j = 0; s2[j] != '\0'; j++)
-    {
-    }
+for (j = 0; s2[j] != '\0'; j++)
+{
+}
 
-  if (n < j)
-    j = n;
+if (n < j)
+j = n;
 
-  j += i;
-  arr = malloc(sizeof(char *) * (j + 1));
+j += i;
+arr = malloc(sizeof(char *) * (j + 1));
 
-  if (arr == NULL)
-    return (NULL);
+if (arr == NULL)
+return (NULL);
 
-  for (co = 0; co < i; co++)
-    arr[co] = s1[co];
-  for (co_2 = 0; co < j; co_2++)
-    {
-      arr[co] = s2[co_2];
-      co++;
-    }
-  co++;
-  arr[co] = '\0';
-  return (arr);
+for (co = 0; co < i; co++)
+arr[co] = s1[co];
+for (co_2 = 0; co < j; co_2++)
+{
+arr[co] = s2[co_2];
+co++;
+}
+co++;
+arr[co] = '\0';
+return (arr);
 }
