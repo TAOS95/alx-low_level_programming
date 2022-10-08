1b9dd2fa35456762664abacdcceca017ac838d31
TAOS95 <tas95245@gmail.com>
2022-10-07 18:01:20 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,10 +6,8 @@
  */
 void *malloc_checked(unsigned int b)
 {
-void *point = malloc(b);
-
-if (point == NULL)
-exit(98);
-  
-return (point);
+  void *point = malloc(b);
+  if (point == NULL)
+    exit(98);
+  return (point);
 }
