d101a7202a12f50d66cfbaa6f6f550c3cc906d74
TAOS95 <tas95245@gmail.com>
2022-10-24 03:54:41 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_listint - function with one argument
+ * print_list_t - function with one argument
  * @h: pointer to struct
  *
  * Description: prints all the elements of a list
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_list_t(const list_t *h)
 {
   int count = 0;
 
